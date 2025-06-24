b82e88f7b7e8262936fe490f61b7e678756e7b85
Akash-singh45 <akuloloo14@gmail.com>
2025-06-24 12:30:09 +0530

    Complete Delete user functionality .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <Provider store={store}>
